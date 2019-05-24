229f71421ee958126eb1ef25a28a4b9fc8d897d3
GuillePLimon <guillermo.palacios@edu.uah.es>
2019-05-24 17:21:29 +0200

    update nivel 3

--- Programación/LilWitch/src/game/Principal.java
+++ Programación/LilWitch/src/game/Principal.java
@@ -39,8 +39,8 @@ public class Principal extends StateBasedGame{
         this.addState(new Intro5());
         this.addState(new Intro6());
         this.addState(new Intro7());
-        this.addState(new Nivel1());*/
-        this.addState(new Nivel2());
+        this.addState(new Nivel1());
+        this.addState(new Nivel2());*/
         this.addState(new Nivel3());
         this.addState(new Nivel4());
         this.addState(new Nivel5());
