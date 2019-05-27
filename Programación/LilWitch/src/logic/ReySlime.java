91cee0f9d879b8ce46b237e2f1ccc6ece98dea82
Alvarikoke99 <48294320+Alvarikoke99@users.noreply.github.com>
2019-05-27 14:24:47 +0200

    Corregido el nombre de nivel5

--- Programación/LilWitch/src/logic/ReySlime.java
+++ Programación/LilWitch/src/logic/ReySlime.java
@@ -32,6 +32,8 @@ public class ReySlime extends Boss {
         avanzar(delta); 
         float x = super.getPosicion().getX();
         float y = super.getPosicion().getX();
+        float pX = player.getPosicion().getX();
+        float pY = player.getPosicion().getY();
         System.out.println("Posicion jugador: " + player.getPosicion().getX() + " , " + player.getPosicion().getY());
         float dirX = player.getPosicion().getX() - x;
         float dirY = player.getPosicion().getY() - y;
@@ -66,10 +68,70 @@ public class ReySlime extends Boss {
             super.setUp(true);
             super.setDown(false);
         }
-        updateAnimation();
+        updateAnimacion();
     }
     
-    public void updateAnimation() {
+    @Override
+    public void avanzar(int delta) {
+        if(super.isColision()) {
+            if(super.isUp()) {
+                movX = (int) (Math.random() * 3 + 1);
+                movY = 1;
+            }
+            if(super.isDown()) {
+                movX = (int) (Math.random() * 3 + 1);
+                movY = 3;
+            }
+            if(super.isR()) {
+                movX = 1;
+                movY = (int) (Math.random() * 3 + 1);
+            }
+            else {
+                movX = 3;
+                movY = (int) (Math.random() * 3 + 1);
+            }
+            super.setColision(false);
+            
+            switch(movX){
+                case 1:
+                    super.getSprite().moverX(-super.getVelocidad() * ((float) delta / 1000));
+                    super.setL(true);
+                    super.setR(false);
+                    break;
+                case 2:
+                    super.getSprite().moverX(0);
+                    super.setL(false);
+                    super.setR(false);
+                    break;
+                case 3:
+                    super.getSprite().moverX(super.getVelocidad() * ((float) delta / 1000));
+                    super.setL(false);
+                    super.setR(true);
+                    break;  
+            }
+            
+            switch(movY){
+                case 1:
+                    super.getSprite().moverY(-super.getVelocidad() * ((float) delta / 1000));
+                    super.setUp(true);
+                    super.setDown(false);
+                    break;
+                case 2:
+                    super.getSprite().moverY(0);
+                    super.setUp(false);
+                    super.setDown(false);
+                    break;
+                case 3:
+                    super.getSprite().moverY(super.getVelocidad() * ((float) delta / 1000));
+                    super.setUp(false);
+                    super.setDown(true);
+                    break;   
+            }
+            updateAnimacion();
+        }
+    }
+    
+    public void updateAnimacion() {
         if (super.isUp()) {
             super.getSprite().stopL();
             super.getSprite().stopR();
