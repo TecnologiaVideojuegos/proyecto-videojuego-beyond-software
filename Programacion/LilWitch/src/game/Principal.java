faf471ea9bc8d909b0287ad5aae6b4c6879d536c
GuillePLimon <guillermo.palacios@edu.uah.es>
2019-05-28 18:37:10 +0200

    Update Principal.java

--- Programacion/LilWitch/src/game/Principal.java
+++ Programacion/LilWitch/src/game/Principal.java
@@ -38,11 +38,11 @@ public class Principal extends StateBasedGame{
         this.addState(new Intro4());
         this.addState(new Intro5());
         this.addState(new Intro6());
-        this.addState(new Intro7());
+        this.addState(new Intro7());*/
         this.addState(new Nivel1());
         this.addState(new Nivel2());
         this.addState(new Nivel3());
-        this.addState(new Nivel4());*/
+        this.addState(new Nivel4());
         this.addState(new Nivel5());
         this.addState(new GameOver());
         this.addState(new Menu());
