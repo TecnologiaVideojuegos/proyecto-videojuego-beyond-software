51bf86ec20bcfb6dd8f17656091d1517c91667cd
GuillePLimon <guillermo.palacios@edu.uah.es>
2019-05-28 18:29:19 +0200

    nivel 5 y sonido

--- Programacion/LilWitch/src/game/Nivel5.java
+++ Programacion/LilWitch/src/game/Nivel5.java
@@ -29,6 +29,8 @@ public class Nivel5 extends BasicGameState{
     private int selected;
     private boolean paused = false;
     private Image image;
+    private Music music;
+    private Sound select;
     
     @Override
     public int getID() {
@@ -43,33 +45,36 @@ public class Nivel5 extends BasicGameState{
         player = new Jugador(proyectiles);
         mapa = new SpriteSheet("resources/niveles/Nivel 5_v1.png", 1920, 1080);
         image = new Image("resources/intro/fondo_5.png");
-        Wall limites_1 = new Wall(new float[]{100, 100, 100, 860, 840, 860, 840, 960, 1080, 960, 1080, 860, 1820, 860, 1820, 600, 1920, 600, 1920, 360, 1820, 360, 1820, 100});
-        Wall limites_2 = new Wall(new float[]{100, 100, 100, 360, 0, 360, 0, 600, 100, 600, 100, 860, 1820, 860, 1820, 600, 1920, 600, 1920, 360, 1820, 360, 1820, 100, 1080, 100, 1080, 0, 840, 0, 840, 100});
-        Wall limites_3 = new Wall(new float[]{100, 100, 100, 360, 0, 360, 0, 600, 100, 600, 100, 860, 1820, 860, 1820, 100});
-        Wall limites_4 = new Wall(new float[]{100, 100, 100, 860, 840, 860, 840, 960, 1080, 960, 1080, 860, 1820, 860, 1820, 100});
-        Wall oscuridad1_1 = new Wall(new float[]{360, 100, 360, 240, 240, 240, 240, 360, 100, 360});
-        Wall oscuridad1_2 = new Wall(new float[]{100, 600, 240, 600, 240, 720, 360, 720, 360, 860, 100, 860});
-        Wall oscuridad1_3 = new Wall(new float[]{1560, 860, 1560, 720, 1680, 720, 1680, 600, 1820, 600, 1820, 860});
-        Wall oscuridad1_4 = new Wall(new float[]{1560, 100, 1560, 240, 1680, 240, 1680, 360, 1820, 360, 1820, 100});
-        Wall oscuridad2_1 = new Wall(new float[]{360, 100, 360, 240, 240, 240, 240, 360, 100, 360});
-        Wall oscuridad2_2 = new Wall(new float[]{100, 600, 240, 600, 240, 720, 360, 720, 360, 860, 100, 860});
-        Wall oscuridad2_3 = new Wall(new float[]{1560, 860, 1560, 720, 1680, 720, 1680, 600, 1820, 600, 1820, 860});
-        Wall oscuridad2_4 = new Wall(new float[]{1560, 100, 1560, 240, 1680, 240, 1680, 360, 1820, 360, 1820, 100});
-        Wall oscuridad2_5 = new Wall(new float[]{840, 360, 840, 480, 720, 480, 720, 600, 840, 600, 840, 720, 1080, 720, 1080, 600, 1200, 600, 1200, 480, 1080, 480, 1080, 360});
-        Wall oscuridad3_1 = new Wall(new float[]{100, 600, 240, 600, 240, 720, 360, 720, 360, 840});
-        Wall oscuridad3_2 = new Wall(new float[]{600, 240, 600, 360, 480, 360, 480, 480, 600, 480, 600, 600, 720, 600, 720, 480, 840, 480, 840, 360, 720, 360, 720, 240});
-        Wall oscuridad3_3 = new Wall(new float[]{1320, 100, 1320, 240, 1560, 240, 1560, 360, 1680, 360, 1680, 480, 1820, 480, 1820, 100 });
-        Wall oscuridad3_4 = new Wall(new float[]{1200, 840,1200, 720, 1080, 720, 1080, 600, 1200, 600, 1200, 480, 1320, 480, 1320, 600, 1440, 600, 1440, 720, 1320, 720, 1320, 840});
-        Wall oscuridad4_1 = new Wall(new float[]{840, 360, 840, 600, 1080, 600, 1080, 360});
-        //Wall oscuridad4_2 = new Wall(new float[]{});
-        //Wall oscuridad4_3 = new Wall(new float[]{});
-        //Wall oscuridad4_4 = new Wall(new float[]{});
-        //Wall oscuridad4_5 = new Wall(new float[]{});
-        Puerta p11 = new Puerta(1915, 360, 5, 240, 1, 2, 0);
-        Puerta p21 = new Puerta(1915, 360, 5, 240, 1, 3, 0);
-        Puerta p22 = new Puerta(0, 360, 5, 240, 3, 1, 0);
-        Puerta p31 = new Puerta(0, 360, 5, 240, 3, 2, 0);
-        Puerta p23 = new Puerta(840, 0, 240, 5, 0, 4, 0);
+        music = new Music("resources/sonidos/Caves.ogg");
+        music.setVolume(0.5f);
+        music.loop();
+        select = new Sound("resources/sonidos/Select.ogg");
+        Wall limites_1 = new Wall(new float[]{20, 20, 20, 940, 840, 940, 840, 960, 1080, 960, 1080, 940, 1900, 940, 1900, 600, 1920, 600, 1920, 360, 1900, 360, 1900, 20});
+        Wall limites_2 = new Wall(new float[]{20, 20, 20, 360, 0, 360, 0, 600, 20, 600, 20, 940, 1900, 940, 1900, 600, 1920, 600, 1920, 360, 1900, 360, 1900, 20});
+        Wall limites_3 = new Wall(new float[]{20, 20, 20, 360, 0, 360, 0, 600, 20, 600, 20, 940, 1900, 940, 1900, 20, 1080, 20, 1080, 0, 840, 0, 840, 20});
+        Wall limites_4 = new Wall(new float[]{20, 20, 20, 940, 840, 940, 840, 960, 1080, 960, 1080, 940, 1900, 940, 1900, 20, 1080, 20, 1080, 0, 840, 0, 840, 20});
+        Wall limites_5 = new Wall(new float[]{20, 20, 20, 940, 1900, 940, 1900, 600, 1920, 600, 1920, 360, 1900, 360, 1900, 20});
+        Wall limites_6 = new Wall(new float[]{20, 20, 20, 940, 840, 940, 840, 960, 1080, 960, 1080, 940, 1900, 940, 1900, 20, 1080, 20, 1080, 0, 840, 0, 840, 20});
+        Wall oscuridad1_1 = new Wall(new float[]{120, 120, 120, 840, 240, 840, 240, 480, 360, 480, 360, 360, 480, 360, 480, 240, 600, 240, 600, 120, 480, 120, 480, 240, 360, 240, 360, 360, 240, 360, 240, 120});
+        Wall oscuridad1_2 = new Wall(new float[]{1200, 120, 1200, 240, 1560, 240, 1560, 120});
+        Wall oscuridad1_3 = new Wall(new float[]{1080, 360, 1080, 720, 1200, 720, 1200, 840, 1560, 840, 1560, 360, 1440, 360, 1440, 720, 1200, 720, 1200, 360});
+        Wall oscuridad2_1 = new Wall(new float[]{240, 120, 240, 360, 360, 360, 360, 480, 240, 480, 240, 600, 360, 600, 360, 480, 480, 480, 480, 120});
+        Wall oscuridad2_2 = new Wall(new float[]{1080, 120, 1080, 600, 1440, 600, 1440, 480, 1560, 480, 1560, 120, 1440, 120, 1440, 480, 1200, 480, 1200, 120});
+        Wall oscuridad3_1 = new Wall(new float[]{240, 120, 240, 600, 360, 600, 360, 360, 480, 360, 480, 240, 720, 240, 720, 120, 480, 120, 480, 240, 360, 240, 360, 120});
+        Wall oscuridad3_2 = new Wall(new float[]{1200, 120, 1200, 480, 1320, 480, 1320, 600, 1560, 600, 1560, 480, 1320, 480, 1320, 360, 1560, 360, 1560, 120});
+        Wall oscuridad6_1 = new Wall(new float[]{600, 360, 600, 480, 1320, 480, 1320, 360});
+        Wall oscuridad6_2 = new Wall(new float[]{600, 600, 600, 720, 1320, 720, 1320, 600});
+        Puerta p1_1 = new Puerta(1915, 360, 5, 240, 1, 2, 0);
+        Puerta p2_1 = new Puerta(1915, 360, 5, 240, 1, 3, 0);
+        Puerta p2_2 = new Puerta(0, 360, 5, 240, 3, 1, 0);
+        Puerta p3_1 = new Puerta(0, 360, 5, 240, 3, 2, 0);
+        Puerta p3_2 = new Puerta(840, 0, 240, 5, 0, 6, 0);
+        Puerta p4_1 = new Puerta(840, 0, 240, 5, 0, 4, 0);
+        Puerta p4_2 = new Puerta(840, 960, 240, 5, 2, 6, 0);
+        Puerta p4_3 = new Puerta(20, 420, 5, 120, 3, 5, 0);
+        Puerta p5_1 = new Puerta(1915, 360, 5, 240, 1, 4, 0);
+        Puerta p6_1 = new Puerta(840, 960, 240, 5, 2, 3, 0);
+        Puerta p6_2 = new Puerta(840, 0, 240, 5, 0, 4, 0);
         
         
         ArrayList<Wall> walls1 = new ArrayList<>();
@@ -77,49 +82,57 @@ public class Nivel5 extends BasicGameState{
         walls1.add(oscuridad1_1);
         walls1.add(oscuridad1_2);
         walls1.add(oscuridad1_3);
-        walls1.add(oscuridad1_4);
         ArrayList<Puerta> puertas1 = new ArrayList<>();
-        puertas1.add(p11);
+        puertas1.add(p1_1);
         
         ArrayList<Wall> walls2 = new ArrayList<>();
         walls2.add(limites_2);
         walls2.add(oscuridad2_1);
         walls2.add(oscuridad2_2);
-        walls2.add(oscuridad2_3);
-        walls2.add(oscuridad2_4);
-        walls2.add(oscuridad2_5);
         ArrayList<Puerta> puertas2 = new ArrayList<>();
-        puertas2.add(p21);
-        puertas2.add(p22);
-        puertas2.add(p23);
+        puertas2.add(p2_1);
+        puertas2.add(p2_2);
         
         ArrayList<Wall> walls3 = new ArrayList<>();
         walls3.add(limites_3);
         walls3.add(oscuridad3_1);
         walls3.add(oscuridad3_2);
-        walls3.add(oscuridad3_3);
-        walls3.add(oscuridad3_4);
         ArrayList<Puerta> puertas3 = new ArrayList();
-        puertas3.add(p31);
+        puertas3.add(p3_1);
+        puertas3.add(p3_2);
         
         ArrayList<Wall> walls4 = new ArrayList<>();
         walls4.add(limites_4);
-        walls4.add(oscuridad4_1);
-        //walls4.add(oscuridad4_2);
-        //walls4.add(oscuridad4_3);
-        //walls4.add(oscuridad4_4);
-        //walls4.add(oscuridad4_5);
+        ArrayList<Puerta> puertas4 = new ArrayList();
+        puertas4.add(p4_1);
+        puertas4.add(p4_2);
+        puertas4.add(p4_3);
         
-        ArrayList<Puerta> puertas4 = new ArrayList(); 
+        ArrayList<Wall> walls5 = new ArrayList<>();
+        walls5.add(limites_5);
+        ArrayList<Puerta> puertas5 = new ArrayList(); 
+        puertas5.add(p5_1);
+        
+        ArrayList<Wall> walls6 = new ArrayList<>();
+        walls6.add(limites_6);
+        walls6.add(oscuridad6_1);
+        walls6.add(oscuridad6_2);
+        ArrayList<Puerta> puertas6 = new ArrayList();
+        puertas6.add(p6_1);
+        puertas6.add(p6_2);
         
         Sala sala1 = new Sala(mapa.getSubImage(0, 1), walls1, puertas1, null, player, proyectiles);
         Sala sala2 = new Sala(mapa.getSubImage(1, 1), walls2, puertas2, null, player, proyectiles);
         Sala sala3 = new Sala(mapa.getSubImage(2, 1), walls3, puertas3, null, player, proyectiles);
         Sala sala4 = new Sala(mapa.getSubImage(1, 0), walls4, puertas4, null, player, proyectiles);
+        Sala sala5 = new Sala(mapa.getSubImage(0, 0), walls5, puertas5, null, player, proyectiles);
+        Sala sala6 = new Sala(mapa.getSubImage(2, 0), walls6, puertas6, null, player, proyectiles);
         salas.add(sala1);
         salas.add(sala2);
         salas.add(sala3);
         salas.add(sala4);
+        salas.add(sala5);
+        salas.add(sala6);
     }
 
     @Override
@@ -175,12 +188,14 @@ public class Nivel5 extends BasicGameState{
         if(paused)
         {
 		if (key == Input.KEY_DOWN) {
+                        select.play();
 			selected++;
 			if (selected >= options.length) {
 				selected = 0;
 			}
 		}
 		if (key == Input.KEY_UP) {
+                        select.play();
 			selected--;
 			if (selected < 0) {
 				selected = options.length - 1;
--- Programacion/LilWitch/src/logic/Jugador.java
+++ Programacion/LilWitch/src/logic/Jugador.java
@@ -20,6 +20,7 @@ public class Jugador implements IColisionable {
     private int vida, vidaTotal, cooldown, varitaActual, tiempoInvencibilidad, flickerTime;
     private Image corazonVacio, corazonLleno, corazonMedio;
     private Inventario inventario;
+    private Sound sonido;
 
     public Jugador(ControladorProyectiles proyectiles) throws SlickException {
         SpriteSheet tileSet;
@@ -59,6 +60,8 @@ public class Jugador implements IColisionable {
         this.inventario = new Inventario(true);
         this.hit = false;
         this.flickerTime = 0;
+        
+        sonido = new Sound("resources/sonidos/Select.ogg");
     }
     
     public void draw(Input entrada, Graphics g) {
@@ -140,9 +143,11 @@ public class Jugador implements IColisionable {
     private void updateTeclado(Input entrada, int delta) {
         if(!stop){
             if(entrada.isKeyPressed(Input.KEY_LCONTROL)) {
+                sonido.play();
                 inventario.cambiarVaritaL();
             }
             if(entrada.isKeyPressed(Input.KEY_SPACE)) {
+                sonido.play();
                 inventario.cambiarVaritaR();
             }
             if(entrada.isKeyPressed(Input.KEY_E)) {
