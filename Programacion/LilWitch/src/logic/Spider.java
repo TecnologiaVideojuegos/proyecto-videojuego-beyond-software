7d45ef3fa3ed405ac559fab4eb40dbbeca973e63
GuillePLimon <guillermo.palacios@edu.uah.es>
2019-06-03 21:28:17 +0200

    Update Spider.java

--- Programacion/LilWitch/src/logic/Spider.java
+++ Programacion/LilWitch/src/logic/Spider.java
@@ -16,7 +16,7 @@ public class Spider extends Boss {
     private int movX, movY, dirXo, dirYo, tiempo;
 
     public Spider(Jugador player) throws SlickException {
-        super("arana.png", 125, 125, 960, 120, 2, 1, 100, player, 30, 30, 60, 35);
+        super("arana_2.png", 300, 300, 960, 120, 2, 1, 100, player, 30, 30, 60, 35);
         this.player = player;
         this.movX = 2;
         this.movY = 2;
