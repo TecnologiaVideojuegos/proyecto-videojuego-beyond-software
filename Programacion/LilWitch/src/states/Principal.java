612b3aef024bd9147f97633547e974824e931d77
Álvaro Barchín Rubio <alvarobarchin@hotmail.es>
2019-06-23 00:09:18 +0200

    Añadidos consejos en las pantallas de carga

--- Programacion/LilWitch/src/states/Principal.java
+++ Programacion/LilWitch/src/states/Principal.java
@@ -23,8 +23,8 @@ public class Principal extends StateBasedGame{
     public Principal() throws SlickException {
         super("Lil'Witch");
         contenedor = new AppGameContainer(this);
-        contenedor.setDisplayMode(1920, 1080, false);
-        //contenedor.setMouseGrabbed(true);
+        contenedor.setDisplayMode(1920, 1080, true);
+        contenedor.setMouseGrabbed(true);
         contenedor.setShowFPS(false);
         contenedor.start();
     }
